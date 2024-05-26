3f9d24715e23b7cc4bc73e5fa4264b08d20a7101
bartekw91 <161207607+bartekw91@users.noreply.github.com>
2024-05-26 15:04:35 +0200

    Więcej o numpy

--- powtorka_z_numpy.py
+++ powtorka_z_numpy.py
@@ -15,3 +15,68 @@ a += b
 print(a)
 a -= b
 print(a)
+
+d = np.arange(3)
+print()
+print(d)
+print(np.exp(d))
+print(np.sqrt(d))
+e = np.array([2., -1., 4.])
+print(np.add(d, e))
+print()
+
+f = a*b
+print(f)
+print(a.dot(b))  # Mnożenie macierzy o tych samych WxK
+g = np.array([[1, 5], [2, 6], [7, 4]])
+h = np.array([[2, 5, 4], [4, 3, 1]])
+print(g)
+print(h)
+print(np.dot(g, h))  # Mnożenie macierzy
+print(np.dot(h, g))
+print()
+i = np.arange(12).reshape(3,4)  # Stwórz wektor od 0 do 11 i zamień na macierz 3x4
+print(i)
+print(i.sum())  # Sumuj wszystkie macierzy
+print(i.sum(axis=0))  # Sumuj liczby po kolumnie
+print(i.min(axis=1))  # Pokaż najmniejsze liczby poszczególnych macierzy przy każdym wierszu
+print(i.cumsum(axis=1))  # Skumulowana suma elementów
+print()
+j = np.arange(6).reshape((3, 2))
+print(j)
+print()
+for q in j:  # Element q będzie jako pojedyńczy wiersz
+    print(q)
+print()
+for q in j.flat:  # Spłaszczenie do wektora
+    print(q)
+print()
+#for q in j:
+#    for r in range(len(j)):  # Dostęp do każdego elementu z wierszy
+#        print(q[r], end=' ')
+print()
+#for q in range(0, j.shape[0]):
+#    for w in range(0, a.shape[1]):
+#        print(j[q][w], end=' ') # Dostęp do poszczególnego elementu WxK
+#        print(' ')
+
+print('PRZEKSZTAŁCENIE')
+k = np.arange(6)
+print(k)
+print(k.shape)
+print('')
+l = k.reshape((2, 3))
+print(l)
+print(l.shape)
+print('')
+m = l.reshape((3, 2))
+print(m)
+print(m.shape)
+print('')
+n = m.ravel()  # Spłaszcz macierz do postaci wektora
+print(n)
+print(n.shape)
+print('')
+o = l.T   # Transponowanie
+print(o)
+print(o.shape)
